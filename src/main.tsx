f0788c7053c309e80726fe43ca5de7149da355a9
yoampies <andresampies@gmail.com>
2025-12-22 12:45:59 -0500

    migrated last files to typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
+// src/main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root')!;
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
